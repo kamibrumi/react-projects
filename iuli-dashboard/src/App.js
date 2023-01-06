698423b9ba2da479c276f2e1f6166438e591d3dc
Camelia D. Brumar <cameliabrumar@gmail.com>
2023-01-05 22:55:17 -0500

    initial commit iuli dashboard

---
+++ iuli-dashboard/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
