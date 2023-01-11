ca54f0095ddddba60071b7074920823805e8bfea
Camelia D. Brumar <cameliabrumar@gmail.com>
2023-01-10 20:17:55 -0500

    eliminated the strict mode tag since it would make everything render twice

--- energy-savings-dashboard/src/components/Barchart.jsx
+++ energy-savings-dashboard/src/components/Barchart.jsx
@@ -13,7 +13,6 @@ const Barchart = () => {
     // append the svg object to the body of the page
     const svg = d3
       .select(ref.current)
-      .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -27,40 +26,34 @@ const Barchart = () => {
     const y = d3.scaleLinear().range([height, 0]);
     const yAxis = svg.append("g").attr("class", "myYaxis");
 
-    // A function that create / update the plot for a given variable:
-    function update(selectedVar) {
-      // Parse the Data
-      d3.csv(
-        "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/barplot_change_data.csv"
-      ).then(function (data) {
-        // X axis
-        x.domain(data.map((d) => d.group));
-        xAxis.transition().duration(1000).call(d3.axisBottom(x));
+    d3.csv(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/barplot_change_data.csv"
+    ).then(function (data) {
+      console.log(data);
+      // X axis
+      x.domain(data.map((d) => d.group));
+      xAxis.transition().duration(1000).call(d3.axisBottom(x));
 
-        // Add Y axis
-        y.domain([0, d3.max(data, (d) => +d[selectedVar])]);
-        yAxis.transition().duration(1000).call(d3.axisLeft(y));
+      // Add Y axis
+      y.domain([0, d3.max(data, (d) => +d["var1"])]);
+      yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
-        // variable u: map data to existing bars
-        const u = svg.selectAll("rect").data(data);
+      // variable u: map data to existing bars
+      const u = svg.selectAll("rect").data(data);
 
-        // update bars
-        u.join("rect")
-          .transition()
-          .duration(1000)
-          .attr("x", (d) => x(d.group))
-          .attr("y", (d) => y(d[selectedVar]))
-          .attr("width", x.bandwidth())
-          .attr("height", (d) => height - y(d[selectedVar]))
-          .attr("fill", "#69b3a2");
-      });
-    }
-
-    // Initialize plot
-    update("var1");
+      // update bars
+      u.join("rect")
+        .transition()
+        .duration(1000)
+        .attr("x", (d) => x(d.group))
+        .attr("y", (d) => y(d["var1"]))
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => height - y(d["var1"]))
+        .attr("fill", "#69b3a2");
+    });
   }, []);
 
-  return <svg width={460} height={400} ref={ref} />;
+  return <svg width={460} height={400} id="barchart" ref={ref} />;
 };
 
 export default Barchart;
